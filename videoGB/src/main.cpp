9c00e7138bf5af7fede8dc09705700bdd5a69539
Liviousss <001livio@gmail.com>
2025-05-26 16:20:32 +0200

    videoGB: fix to main

--- videoGB/src/main.cpp
+++ videoGB/src/main.cpp
@@ -4,7 +4,9 @@
 #include "../header/gaussian_blur.h"
 
 
-int BlurVideo(std::string src,std::string dest);
+enum Mode {BASIC,STREAMS,SHARED_MEMORY};
+
+int BlurVideo(std::string src,std::string dest, Mode mode);
 
 
 int main() {
@@ -12,18 +14,18 @@ int main() {
     std::string videosd_src = "./videos/sd_video.mp4";
     std::string videosd_dest = "./videos/sd_blurred_video.mp4";
 
-    BlurVideo(videosd_src,videosd_dest);
+    BlurVideo(videosd_src,videosd_dest,BASIC);
     
     std::string video720p_src = "./videos/720p_video.mp4";
     std::string video720p_dest = "./videos/720p_blurred_video.mp4";
 
-    BlurVideo(video720p_src,video720p_dest);
+    BlurVideo(video720p_src,video720p_dest, BASIC);
     
     return 0;
 }
 
 
-int BlurVideo(std::string src,std::string dest){
+int BlurVideo(std::string src,std::string dest, Mode mode){
     //Open a video
     cv::VideoCapture cap(src);
     if (!cap.isOpened()) {
@@ -54,26 +56,69 @@ int BlurVideo(std::string src,std::string dest){
     //Create structures for the gaussian blur
     Video video = Video(frameWidth,frameHeight,channels,frames.size(),frames);
     GaussianBlur GB = GaussianBlur();
+        
+    Video blurredVideo(0,0,0,0,std::vector<unsigned char*>(0));
+    printf("-------------------\n");
+    switch (mode)
+    {
+    case Mode::BASIC:
+        {
+            int dataTransferTime = -1, computationTime = -1;
+            blurredVideo = GB.blurVideoGPU(video,&dataTransferTime,&computationTime);
+            if(dataTransferTime!=-1 && computationTime!=-1){
+                printf("GPU data transfer time : %d milliseconds\n",dataTransferTime);
+                printf("GPU computation time : %d milliseconds\n",computationTime);
+            }
+            else{
+                printf("Something went wrong with basic video blur function\n");
+                return -1;
+            }
+            break;
+        }
 
-    //Use the GPU to blur the video
-    int dataTransferTime = -1, computationTime = -1, 
-        totalTimeWithStreams = -1,
-        dataTransferTimeWithSharedMem = -1, computationTimeWithSharedMem = -1;
-    Video blurredVideo = GB.blurVideoGPU(video,&dataTransferTime,&computationTime);
-    Video blurredVideoWithStreams = GB.blurVideoGPUusingStreams(video,&totalTimeWithStreams);
-    Video blurredVideoWithSharedMem = GB.blurVideoGPUusingSharedMemory(video,&dataTransferTimeWithSharedMem,&computationTimeWithSharedMem);
+    case Mode::STREAMS:
+        {
+            int totalTimeWithStreams = -1;
+            blurredVideo = GB.blurVideoGPUusingStreams(video,&totalTimeWithStreams);
+            if(totalTimeWithStreams!=-1){
+                printf("GPU total time using streams: %d milliseconds\n",totalTimeWithStreams);
+            }
+            else{
+                printf("Something went wrong with video blur using streams\n");
+                return -1;
+            }
+            break;
+        }
+        
+
+    case Mode::SHARED_MEMORY:
+        {
+            int dataTransferTimeWithSharedMem = -1, computationTimeWithSharedMem = -1;
+            blurredVideo = GB.blurVideoGPUusingSharedMemory(video,&dataTransferTimeWithSharedMem,&computationTimeWithSharedMem);
+            if(dataTransferTimeWithSharedMem!=-1 && computationTimeWithSharedMem!=-1){
+                printf("GPU data transfer time using shared memory: %d milliseconds\n",dataTransferTimeWithSharedMem);
+                printf("GPU computation time using shared memory: %d milliseconds\n",computationTimeWithSharedMem);
+            }
+            else{
+                printf("Something went wrong with video blur using shared memory\n");
+                return -1;
+            }
+            break;
+        }
+        
+    }
+    printf("-------------------\n");
 
     //Create video writer
     cv::VideoWriter writer(dest,
                     cv::VideoWriter::fourcc('m','p','4','v'),
                     fps,
                     cv::Size(frameWidth,frameHeight));
-
     if(!writer.isOpened()){
         std::cerr << "Error: Cannot open dest video! Path: "<< dest << std::endl;
         return -1;
     }
-    
+
     //write the video
     int i=0;
     while(i<blurredVideo.getFrames()){
@@ -86,32 +131,6 @@ int BlurVideo(std::string src,std::string dest){
     // Release resources
     cap.release();
     cv::destroyAllWindows();
-
-    printf("-------------------\n");
-    if(dataTransferTime!=-1 && computationTime!=-1){
-        printf("GPU data transfer time : %d milliseconds\n",dataTransferTime);
-        printf("GPU computation time : %d milliseconds\n",computationTime);
-    }
-    else{
-        printf("Something went wrong with basic video blur function\n");
-    }
-    
-    if(totalTimeWithStreams!=-1){
-        printf("GPU total time using streams: %d milliseconds\n",totalTimeWithStreams);
-    }
-    else{
-        printf("Something went wrong with video blur using streams\n");
-    }
-
-    if(dataTransferTimeWithSharedMem!=-1 && computationTimeWithSharedMem!=-1){
-        printf("GPU data transfer time using shared memory: %d milliseconds\n",dataTransferTimeWithSharedMem);
-        printf("GPU computation time using shared memory: %d milliseconds\n",computationTimeWithSharedMem);
-    }
-    else{
-        printf("Something went wrong with video blur using shared memory\n");
-    }
-
-    printf("-------------------\n");
 
     return 0;
 
